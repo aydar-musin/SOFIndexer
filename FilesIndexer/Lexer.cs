74153a31798e7f9f7fa82e1f3f408480caff9e7a
Elnar <elideveloper@yandex.ru>
2015-03-30 17:49:10 +0300

    Lexer complete

---
+++ FilesIndexer/Lexer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace FilesIndexer
+{
+    class Lexer
+    {
+
+        string fileName;
+        public Lexer(string fName) { fileName = fName; }
+
+        public string[] getTokens()
+        {
+            // Open the file and read it back.
+            using (System.IO.StreamReader sr = System.IO.File.OpenText(fileName))
+            {
+                string s = sr.ReadToEnd();
+
+                s = s.Replace("'", " ' ").Replace("\"", " \" ").Replace(":", " : ").Replace(";", " ; ").Replace("^", " ^ ")
+                    .Replace(",", " , ").Replace("~", " ~ ").Replace("/", " / ")
+                    .Replace("*", " * ").Replace("+", " + ").Replace("-", " - ").Replace("=", " = ").Replace("<", " < ")
+                    .Replace(">", " > ").Replace("%", " % ")
+                    .Replace("!", " ! ").Replace("(", " ( ").Replace(")", " ) ").Replace("[", " [ ").Replace("]", " ] ")
+                    .Replace("|", " | ").Replace("&", " & ").Replace("#", " # ").Replace("@", " @ ").Replace("$", " $ ")
+                    .Replace("?", " ? ").Replace(".", " . ").Replace("`", " ` ");
+              
+
+                char[] delimiterChars = { '\n', '\r', ' ', '\t', '\v' };
+                string[] lines = s.Split(delimiterChars);
+                List<string> filledLines = new List<string>();
+
+                foreach (string line in lines)
+                {
+                    if (line.Length > 0)
+                    {
+                        filledLines.Add(line);
+                    }
+                }
+
+                foreach (string line in filledLines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                return filledLines.ToArray();
+
+            }
+        }
+
+
+    }
+}
+
